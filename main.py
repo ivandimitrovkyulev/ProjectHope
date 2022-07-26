533b9950aa97b48c29f4af8366fa1933a105f14a
IK <ivandkyulev@gmail.com>
2022-07-26 01:45:31 +0100

    Working script

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from time import (
 )
 
 from src.projecthope.common.exceptions import exit_handler
-from src.projecthope.one_inch import check_arb
+from src.projecthope.one_inch import alert_arb
 from src.projecthope.common.variables import time_format
 
 
@@ -24,16 +24,26 @@ register(exit_handler, program_name)
 # Fetch variables
 info = json.loads(sys.argv[-1])
 timestamp = datetime.now().astimezone().strftime(time_format)
-print(f"{timestamp} - Started screening:")
 
 base_token = "USDC"
-arb_token = "gOHM"
+base_tokens = [token for token in info['base_tokens']]
+arb_tokens = [token for token in info['arb_tokens']]
+
+
+print(f"{timestamp} - Started screening:")
+for arb_token in arb_tokens:
+    arb_token_networks = [net for net in info['arb_tokens'][arb_token]['networks']
+                          if net in info['base_tokens'][base_token]['networks']]
+    print(f"{arb_token} on {arb_token_networks}")
+
 
 loop_counter = 1
 while True:
     start = perf_counter()
 
-    check_arb(info, base_token, arb_token)
+    for arb_token in arb_tokens:
+        alert_arb(info, base_token, arb_token)
+
     sleep(10)
 
     print(f"Loop {loop_counter} executed in {(perf_counter() - start):,.2f} secs.")
--- src/projecthope/one_inch.py
+++ src/projecthope/one_inch.py
@@ -78,22 +78,21 @@ def get_swapout(network_id: str, from_token: tuple, to_token: tuple, amount_floa
     gas = data['estimatedGas']
 
     return {
-        "network": network_name, "gas": gas,
+        "network": network_name, "network_id": network_id, "gas": gas,
         "from_token": {"token": from_token_name, "amount": amount_float, "decimals": from_token_decimal},
         "to_token": {"token": to_token_name, "amount": swap_out_float, "decimals": to_token_decimal}
     }
 
 
-def check_arb(data: dict, base_token: str, arb_token: str) -> None:
+def compare_swaps(data: dict, base_token: str, arb_token: str) -> tuple:
     """
-    Checks 1inch supported blockchains for arbitrage between 2 tokens and alerts via Telegram.
+    Checks 1inch supported blockchains for arbitrage between 2 tokens.
 
     :param data: Input dictionary data
     :param base_token: Name of Base token being swapped in
     :param arb_token: Name of token being Arbitraged
+    :return: Dictionary of Swap_ab & Swap_ba data
     """
-    min_arb = data['arb_tokens'][arb_token]['min_arb']
-
     # Query networks for Base->Arb swap out
     args_ab = parse_args(data, base_token, arb_token)
     with ThreadPoolExecutor(max_workers=len(args_ab)) as pool:
@@ -108,26 +107,44 @@ def check_arb(data: dict, base_token: str, arb_token: str) -> None:
         results = pool.map(lambda p: get_swapout(*p), args_ba, timeout=30)
 
     # Get the maximum swap out
-    max_swap_ba, max_amount_ba = max_swap(results)
+    max_swap_ba, _ = max_swap(results)
 
-    base_round = int(max_swap_ab['from_token']['decimals'] / 3)
-    arb_round = int(max_swap_ab['to_token']['decimals'] / 3)
+    return max_swap_ab, max_swap_ba
 
-    base_swap_in = round(max_swap_ab['from_token']['amount'], base_round)
-    arb_swap_out = round(max_swap_ab['to_token']['amount'], arb_round)
-    network_1 = max_swap_ab['network']
-    base_swap_out = round(max_swap_ba['to_token']['amount'], base_round)
-    arb_swap_in = round(max_swap_ba['from_token']['amount'], arb_round)
-    network_2 = max_swap_ba['network']
 
-    arbitrage = max_amount_ba - base_swap_in
+def alert_arb(data: dict, base_token: str, arb_token: str) -> None:
+    """
+    Alerts via Telegram for arbitrage if found.
+
+    :param data: Input dictionary data
+    :param base_token: Name of Base token being swapped in
+    :param arb_token: Name of token being Arbitraged
+    """
+    # Get arbitrage data
+    swap_ab, swap_ba = compare_swaps(data, base_token, arb_token)
+
+    min_arb = data['arb_tokens'][arb_token]['min_arb']
+
+    base_round = int(swap_ab['from_token']['decimals'] / 3)
+    arb_round = int(swap_ab['to_token']['decimals'] / 3)
+
+    base_swap_in = round(swap_ab['from_token']['amount'], base_round)
+    arb_swap_out = round(swap_ab['to_token']['amount'], arb_round)
+    network_1 = swap_ab['network']
+    network_1_id = swap_ab['network_id']
+    base_swap_out = round(swap_ba['to_token']['amount'], base_round)
+    arb_swap_in = round(swap_ba['from_token']['amount'], arb_round)
+    network_2 = swap_ba['network']
+
+    arbitrage = base_swap_out - base_swap_in
     arbitrage = round(arbitrage, base_round)
+
     if arbitrage >= min_arb:
         timestamp = datetime.now().astimezone().strftime(time_format)
         telegram_msg = f"{timestamp}\n" \
                        f"1. Sell {base_swap_in:,} {base_token} for {arb_swap_out:,} {arb_token} on {network_1}\n" \
                        f"2. Sell {arb_swap_in:,} {arb_token} for {base_swap_out:,} {base_token} on {network_2}\n" \
-                       f"<a href='https://app.1inch.io/#/1/swap/{base_token}/{arb_token}'>" \
+                       f"<a href='https://app.1inch.io/#/{network_1_id}/swap/{base_token}/{arb_token}'>" \
                        f"-->Arbitrage: {arbitrage:,} {base_token}</a>"
 
         terminal_msg = f"1. Sell {base_swap_in:,} {base_token} for {arb_swap_out:,} {arb_token} on {network_1}\n" \
