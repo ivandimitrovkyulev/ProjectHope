c40e842b0389f6ed00f0cd6dacf60a699cfbd32a
IK <ivandkyulev@gmail.com>
2022-07-31 23:39:32 +0100

    Container checker fixed

--- container_check.py
+++ container_check.py
@@ -6,13 +6,48 @@ import os
 import sys
 import time
 import datetime
+import requests
+from re import compile
 
-from src.projecthope.common.message import telegram_send_message
-from src.projecthope.common.variables import (
-    CHAT_ID_DEBUG,
-    time_format,
-    time_format_regex,
-)
+
+def telegram_send_message(message_text: str) -> requests.Response or None:
+    """Sends a Telegram message to a specified chat."""
+    telegram_token: str = ""
+    telegram_chat_id: str = ""
+
+    message_text = str(message_text)
+    env_text = os.popen("cat .env").read()
+
+    values = [val for val in env_text.split("\n") if val != ""]
+    for val in values:
+        if "TOKEN" in val:
+            telegram_token = val.split("=")[1]
+        elif "CHAT_ID_DEBUG" in val:
+            telegram_chat_id = val.split("=")[1]
+
+    if telegram_token == "" or telegram_chat_id == "":
+        raise Exception("Invalid Telegram Token or Chat ID!")
+
+    # construct url using token for a sendMessage POST request
+    url = f"https://api.telegram.org/bot{telegram_token}/sendMessage"
+
+    # Construct data for the request
+    payload = {"chat_id": telegram_chat_id, "text": message_text,
+               "disable_web_page_preview": False, "parse_mode": "HTML"}
+
+    # send the POST request
+    try:
+        # If too many requests, wait for Telegram's rate limit
+        while True:
+            post_request = requests.post(url=url, data=payload, timeout=15)
+
+            if post_request.json()['ok']:
+                return post_request
+
+            time.sleep(3)
+
+    except ConnectionError:
+        return None
 
 
 if len(sys.argv) != 2:
@@ -20,8 +55,13 @@ if len(sys.argv) != 2:
 
 wait_time = 15 * 60
 time.sleep(300)
-container_name = sys.argv[-1]
+
 current_dir = os.getcwd()
+CHAT_ID = "-772766575"
+time_format = "%Y-%m-%d %H:%M:%S, %Z"
+time_format_regex = compile("\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}, [A-Za-z]*")
+
+container_name = sys.argv[-1]
 
 
 while True:
@@ -44,7 +84,7 @@ while True:
         message = f"WARNING - {now_time}\n" \
                   f"Container: {container_name}, path: {current_dir} has stopped!"
         # Send Telegram message in Debug Chat
-        telegram_send_message(message, telegram_chat_id=CHAT_ID_DEBUG)
+        telegram_send_message(message)
 
         break
 
